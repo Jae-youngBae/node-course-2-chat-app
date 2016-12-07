d66806dbbb101456446c6d6deb3e3e4c5536f0ec
jy.bae <bjy@jahasmart.com>
2016-12-07 15:13:38 +0900

    Add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -13,31 +14,27 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-
   // socket.emit from Admin text Welcome to the chat app
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
   // socket.broadcast.emit from Admin text New user joined
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });
-  socket.on('createMessage', (message) => {
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is  from the server');
     // io.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-  })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('Create Email', newEmail);
-  })
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
