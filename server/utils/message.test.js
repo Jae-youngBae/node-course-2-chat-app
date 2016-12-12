6d5eefeba36fc9d4bd659c9b2b3c4ebeab10da7b
jy.bae <bjy@jahasmart.com>
2016-12-12 09:55:00 +0900

    Add Users class and updateUserList event

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage, generateLocationMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -14,13 +14,14 @@ describe('generateMessage', () => {
 });
 
 describe('generateLocationMessage', () => {
-  it('should gnerate correct location object', () => {
-      var from = 'Deb',
-      var latitude = 15;
-      var longitude = 30;
-      var url = "https://www.google.com/maps?q=15,30";
-      var message = generateLocationMessage(from, latitude, longitude);
-      expect(message.createdAt).toBeA('number');
-      expect(message).toInclude({from, url});
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
